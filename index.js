2516c4917052ff514b3c44d0151503baf4164eb1
Rodrigo Prates <rodrigo.prates@indicium.tech>
2026-03-09 22:52:10 -0300

    up code prompt

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ console.log("Hello World");
 let numero = 3
 // printando o número na tela
 console.log(numero)
+
+/*
+criando uma variável constante denominada "prompt" que vai chamar a função require,
+com o argumento "prompt-sync". Isto será usado para solicitar inputs do usuário
+*/
+const prompt = require("prompt-sync")();
+
+/*
+solicitando ao usuário que informe o seu nome,
+de forma com que este nome será atribuído a variável "nome"
+*/
+let nome = prompt("Informe seu nome: ");
+
+//exibindo o nome digitado pelo usuário na tela
+console.log("O nome informado foi: "+nome);
