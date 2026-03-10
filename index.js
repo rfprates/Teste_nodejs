75c8a0c0ce38934144b3bb4dd9570f016225cb93
Rodrigo Prates <rodrigo.prates@indicium.tech>
2026-03-09 22:28:13 -0300

    up code

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+// printando "Hello World" na tela
+console.log("Hello World");
+
+// criando uma variável chamada numero e atribuindo o valor 3 para ela
+let numero = 3
+// printando o número na tela
+console.log(numero)
